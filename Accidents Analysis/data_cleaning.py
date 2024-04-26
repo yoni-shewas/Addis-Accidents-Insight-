766476d72e16a2569dc3c4dd0eee8f67a8b4d9bf
yoniShewa <yonatanmekuriya@gmail.com>
2024-04-26 09:00:40 +0300

    now data is cleaned

--- Accidents Analysis/data_cleaning.py
+++ Accidents Analysis/data_cleaning.py
@@ -1,22 +1,35 @@
+import os
 import pandas as pd
 
-RTAraw = "../data/RTA-Dataset.csv"
+RTAraw = "data/RTA-Dataset.csv"
 
 
 def clean_data(filename):
     try:
         # read csv file
-        df = pd.read_csv(filename)
+        df = pd.read_csv(filename, na_values=[
+                         'na', 'NA', 'N/A', 'NaN', 'Unknown'])
 
         # remove missing and duplicates value
         df = df.dropna()
         df = df.drop_duplicates()
         df = df.reset_index(drop=True)
 
-        df = df.drop('Cleaning (1 or 0)', axis=1)
+        to_drop = [
+            'Sex_of_casualty',
+        ]
+
+        # Remove specified columns without inplace=True
+        df.drop(columns=to_drop, inplace=True)
+
+        # Create directory if it doesn't exist
+        cleaned_dir = "../cleaned_data"
+        if not os.path.exists(cleaned_dir):
+            os.makedirs(cleaned_dir)
 
         # Save cleaned data to a new CSV file
-        cleaned_filename = f"cleaned_data/{filename}"
+        cleaned_filename = os.path.join(
+            cleaned_dir, os.path.basename("RTA-Dataset-cleaned.csv"))
         df.to_csv(cleaned_filename, index=False)
         print("Data cleaned and saved to cleaned_data")
 
